c7b3700f3b04e8a273e3830fb631716450b7eaac
Niklas Salmoukas <niklas@salmoukas.com>
2016-07-05 04:19:46 +0200

    enhance build procedure

--- build.sh
+++ build.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 set -e
+ROOTDIR=`pwd`
 
-./buildlibs.sh
+# build dependencies
+./builddeps.sh
+
+# build program and library
 make
+
+# build node module
+cd "$ROOTDIR/node"
+npm install
+npm build .
+
+# build tests
+cd "$ROOTDIR/tests/node"
+npm install
+npm run build-once
+
+# done
+cd "$ROOTDIR"
--- node/binding.gyp
+++ node/binding.gyp
@@ -7,7 +7,10 @@
             ],
             "include_dirs": [
                 "<!(node -e \"require('nan')\")"
-            ]
+            ],
+            "cflags": [
+                "-std=c++11"
+            ],
         }
     ]
 }
