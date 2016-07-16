867730b679811ba12442c842d1284f11c9b2230e
Niklas Salmoukas <niklas@salmoukas.com>
2016-07-17 01:18:38 +0200

    enhance nodejs test

--- tests/node/test.js
+++ tests/node/test.js
@@ -34,6 +34,10 @@ var exists = function(path) {
   }
 };
 
+process.chdir(__dirname + '/../data');
+console.log('data directory: ' + process.cwd());
+
+console.log('deleting old files...');
 if(exists('npp-6.9.1.img')) fs.unlinkSync('npp-6.9.1.img');
 if(exists('npp-6.9.2.img')) fs.unlinkSync('npp-6.9.2.img');
 if(exists('npp-6.9.1.sig')) fs.unlinkSync('npp-6.9.1.sig');
@@ -41,11 +45,14 @@ if(exists('npp-6.9.1-2.patch')) fs.unlinkSync('npp-6.9.1-2.patch');
 if(exists('npp-6.9.2.img-reconstructed')) fs.unlinkSync('npp-6.9.2.img-reconstructed');
 if(exists('npp-6.9.2-reconstructed')) rmdir('npp-6.9.2-reconstructed');
 
+console.log('creating base and target image...');
 snapsync.snap.create('npp-6.9.1', 'npp-6.9.1.img');
 snapsync.snap.create('npp-6.9.2', 'npp-6.9.2.img');
 
+console.log('syncing images...');
 snapsync.sync.signature('npp-6.9.1.img', 'npp-6.9.1.sig', 0, 8);
 snapsync.sync.delta('npp-6.9.1.sig', 'npp-6.9.2.img', 'npp-6.9.1-2.patch');
 snapsync.sync.patch('npp-6.9.1-2.patch', 'npp-6.9.1.img', 'npp-6.9.2.img-reconstructed');
 
+console.log('extract target image...');
 snapsync.snap.extract('npp-6.9.2.img-reconstructed', 'npp-6.9.2-reconstructed');
