5190a7d553dcff69913880bde6954e1c0b087808
Niklas Salmoukas <niklas@salmoukas.com>
2016-07-16 23:07:42 +0200

    simplify build script

--- build.sh
+++ build.sh
@@ -3,21 +3,13 @@
 set -e
 ROOTDIR=`pwd`
 
-# build dependencies
-./builddeps.sh
-
 # build program and library
 make
 
 # build node module
 cd "$ROOTDIR/node"
 npm install
-npm build .
-
-# build tests
-cd "$ROOTDIR/tests/node"
-npm install
-npm run build-once
+node-gyp rebuild
 
 # done
 cd "$ROOTDIR"
