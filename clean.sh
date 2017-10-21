12eab0506d7b5013e905a8ce9a1742f3f45b1081
Niklas Salmoukas <niklas@hofmann.io>
2017-10-21 22:00:33 +0200

    improve clean script

--- clean.sh
+++ clean.sh
@@ -5,3 +5,9 @@ ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd "$ROOTDIR"
 git clean -f -d -x
+
+cd "$ROOTDIR/librsync"
+git clean -f -d -x
+
+cd "$ROOTDIR/cryptopp"
+git clean -f -d -x
