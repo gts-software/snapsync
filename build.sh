19bff222ef0cab13beb501e43f6327c10b946226
Niklas Salmoukas <niklas@hofmann.io>
2017-10-21 21:50:56 +0200

    improve build process

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ then
 fi
 
 # build process
-if [ "$1" == "--direct" ] || [ "$IN_CONTAINER" == "true" ];
+if [ "$1" == "--direct" ] || [ "$1" == "-d" ] || [ "$IN_CONTAINER" == "true" ];
 then
   cd "$ROOTDIR"
   ./builddeps.sh
@@ -22,5 +22,10 @@ else
   docker build -f build.dockerfile -t snapsync-build-environment:latest .
 
   # run container
-  docker run -it --rm -v "$ROOTDIR":/workspace -u $(id -u):$(id -g) --net=host snapsync-build-environment:latest /workspace/build.sh
+  if [ "$1" == "--shell" ] || [ "$1" == "-s" ];
+  then
+    docker run -it --rm -v "$ROOTDIR":/workspace -u $(id -u):$(id -g) --net=host snapsync-build-environment:latest /bin/bash
+  else
+    docker run -it --rm -v "$ROOTDIR":/workspace -u $(id -u):$(id -g) --net=host snapsync-build-environment:latest /workspace/build.sh
+  fi
 fi
