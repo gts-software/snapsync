07c4cc63d3ed27bc7a9debbd06cd6b3e976deb98
Niklas Salmoukas <niklas@hofmann.io>
2017-10-21 21:49:00 +0200

    improve build system

--- build.dockerfile
+++ build.dockerfile
@@ -4,21 +4,8 @@ FROM node:8-alpine as build
 # install required build tools and libraries
 RUN apk add --no-cache bash make cmake gcc g++ boost boost-dev perl
 
-# prepare working directory
-RUN mkdir -p /build
-WORKDIR /build
+# prepare workspace
+RUN mkdir -p /workspace
 
-# copy and build dependencies
-COPY ./librsync/      /build/librsync/
-COPY ./cryptopp/      /build/cryptopp/
-COPY ./builddeps.sh   /build/builddeps.sh
-
-RUN /build/builddeps.sh
-
-# copy and build libraries
-COPY ./src/           /build/src/
-COPY ./node/          /build/node/
-COPY ./buildlibs.sh   /build/buildlibs.sh
-COPY ./Makefile       /build/Makefile
-
-RUN /build/buildlibs.sh
+# run bash per default
+CMD [ "/bin/bash" ]
--- build.sh
+++ build.sh
@@ -3,6 +3,24 @@
 set -e
 ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# initiate build container
-cd "$ROOTDIR"
-docker build -f build.dockerfile .
+# check if we are runnin in an container
+IN_CONTAINER=false
+if cat /proc/1/cgroup | grep docker > /dev/null;
+then
+  IN_CONTAINER=true
+fi
+
+# build process
+if [ "$1" == "--direct" ] || [ "$IN_CONTAINER" == "true" ];
+then
+  cd "$ROOTDIR"
+  ./builddeps.sh
+  ./buildlibs.sh
+else
+  # build container
+  cd "$ROOTDIR"
+  docker build -f build.dockerfile -t snapsync-build-environment:latest .
+
+  # run container
+  docker run -it --rm -v "$ROOTDIR":/workspace -u $(id -u):$(id -g) --net=host snapsync-build-environment:latest /workspace/build.sh
+fi
